f4fee01b00474dcb0c45cdba7df67f3f82e8cc22
Unknown <alekcyvolkov@gmail.com>
2017-11-20 20:53:50 +0300

    change db connection and index.php

--- views/site/index.php
+++ views/site/index.php
@@ -1,53 +1,34 @@
 <?php
 
 /* @var $this yii\web\View */
+use yii\bootstrap\Html;
 
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
+<?php
+    $blocks = [
+    [
+    'title' => 'Кафедры',
+    'description' => 'Управление кафедрами',
+    'label' => 'Кафедры',
+    'icon' => 'cog',
+    'url' => ['/config/index'],
+    ]
+    ];
+    ?>
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    <?php foreach ($blocks as $number => $block): ?>
+    <?php if ($number % 3 == 0): ?>
+    <?php if ($number != 0): ?>
+</div>
+<?php endif; ?>
+<div class="row">
+    <?php endif; ?>
+    <div class="col-lg-4">
+        <h3><?= $block['title'] ?></h3>
+        <p><?= $block['description'] ?></p>
+        <p><?= Html::a(Html::icon($block['icon']) . ' ' . $block['label'], $block['url'], ['class' => 'btn btn-default']) ?></p>
     </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
-    </div>
+    <?php endforeach; ?>
 </div>
