854886015c6cc1754b08486e1ae43f6e2a011a8e
Unknown <alekcyvolkov@gmail.com>
2017-12-01 12:11:24 +0300

    change main panel

--- views/site/index.php
+++ views/site/index.php
@@ -9,89 +9,92 @@ $this->title = 'My Yii Application';
 <?php
     $blocks = [
         [
-            'title' => 'Кафедры',
-            'description' => 'Управление кафедрами',
-            'label' => 'Кафедры',
-            'icon' => '',
-            'url' => ['/chair/index'],
+            [
+                'title' => 'Факультеты',
+                'description' => 'Управление факультетами',
+                'label' => 'Факультеты',
+                'icon' => '',
+                'url' => ['/faculty/index'],
+            ],
+            [
+                'title' => 'Специальности',
+                'description' => 'Управление специальностями',
+                'label' => 'Специальности',
+                'icon' => '',
+                'url' => ['/speciality/index'],
+            ],
         ],
         [
-            'title' => 'Группы',
-            'description' => 'Управление группами',
-            'label' => 'Группы',
-            'icon' => '',
-            'url' => ['/groups/index'],
-        ],
-        [
-            'title' => 'Факультеты',
-            'description' => 'Управление факультетами',
-            'label' => 'Факультеты',
-            'icon' => '',
-            'url' => ['/faculty/index'],
-        ],
-        [
-            'title' => 'Специальности',
-            'description' => 'Управление специальностями',
-            'label' => 'Специальности',
-            'icon' => '',
-            'url' => ['/speciality/index'],
-        ],
-        [
-            'title' => 'Вид оценки',
-            'description' => 'Управление оценками',
-            'label' => 'Вид оценки',
-            'icon' => '',
-            'url' => ['/mark/index'],
-        ],
-        [
-            'title' => 'Вид отчетности',
-            'description' => 'Управление типом отчетности',
-            'label' => 'Вид отчетности',
-            'icon' => '',
-            'url' => ['/reporting-type/index'],
-        ],
-        [
-            'title' => 'Студенты',
-            'description' => 'Управление студентами',
-            'label' => 'Студенты',
-            'icon' => '',
-            'url' => ['/student/index'],
-        ],
-        [
-            'title' => 'Преподаватели',
-            'description' => 'Управление преподавателями',
-            'label' => 'Преподаватели',
-            'icon' => '',
-            'url' => ['/teacher/index'],
-        ],
-        [
-            'title' => 'Предметы',
-            'description' => 'Управление предметами',
-            'label' => 'Предметы',
-            'icon' => '',
-            'url' => ['/subject/index'],
-        ],
-        [
-            'title' => 'Успеваемость',
-            'description' => 'Управление успеваемостью',
-            'label' => 'Успеваемость',
-            'icon' => '',
-            'url' => ['/academic-performance/index'],
+            [
+                'title' => 'Кафедры',
+                'description' => 'Управление кафедрами',
+                'label' => 'Кафедры',
+                'icon' => '',
+                'url' => ['/chair/index'],
+            ],
+            [
+                'title' => 'Группы',
+                'description' => 'Управление группами',
+                'label' => 'Группы',
+                'icon' => '',
+                'url' => ['/groups/index'],
+            ],
+            [
+                'title' => 'Вид оценки',
+                'description' => 'Управление оценками',
+                'label' => 'Вид оценки',
+                'icon' => '',
+                'url' => ['/mark/index'],
+            ],
+            [
+                'title' => 'Вид отчетности',
+                'description' => 'Управление типом отчетности',
+                'label' => 'Вид отчетности',
+                'icon' => '',
+                'url' => ['/reporting-type/index'],
+            ],
+            [
+                'title' => 'Студенты',
+                'description' => 'Управление студентами',
+                'label' => 'Студенты',
+                'icon' => '',
+                'url' => ['/student/index'],
+            ],
+            [
+                'title' => 'Преподаватели',
+                'description' => 'Управление преподавателями',
+                'label' => 'Преподаватели',
+                'icon' => '',
+                'url' => ['/teacher/index'],
+            ],
+            [
+                'title' => 'Предметы',
+                'description' => 'Управление предметами',
+                'label' => 'Предметы',
+                'icon' => '',
+                'url' => ['/subject/index'],
+            ],
+            [
+                'title' => 'Успеваемость',
+                'description' => 'Управление успеваемостью',
+                'label' => 'Успеваемость',
+                'icon' => '',
+                'url' => ['/academic-performance/index'],
+            ],
         ],
     ];
     ?>
 
+
 <?php foreach ($blocks as $number => $block): ?>
-    <?php if ($number % 3 == 0): ?>
-    <?php if ($number != 0): ?>
-    </div>
-    <?php endif; ?>
     <div class="row">
-    <?php endif; ?>
-    <div class="col-lg-4">
-        <h3><?= $block['title'] ?></h3>
-        <p><?= $block['description'] ?></p>
-        <p><?= Html::a(Html::icon($block['icon']) . ' ' . $block['label'], $block['url'], ['class' => 'btn btn-default']) ?></p>
-    </div>
+    <?php foreach ($block as $item):?>
+            <div class="col-lg-4">
+                <h3><?= $item['title'] ?></h3>
+                <p><?= $item['description'] ?></p>
+                <p><?= Html::a(Html::icon($item['icon']) . ' ' . $item['label'], $item['url'], ['class' => 'btn btn-default']) ?></p>
+            </div>
+    <?php endforeach;?>
+    </div><hr>
     <?php endforeach; ?>
-</div>
+
