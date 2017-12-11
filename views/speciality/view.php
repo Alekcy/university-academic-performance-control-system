0f78998836fc071064638ffaf9c105343fcc2d25
Unknown <alekcyvolkov@gmail.com>
2017-12-11 21:36:16 +0300

    fix titles names

--- views/speciality/view.php
+++ views/speciality/view.php
@@ -12,8 +12,6 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="speciality-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
     <p>
         <?= Html::a('Обновить', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
         <?= Html::a('Удалить', ['delete', 'id' => $model->id], [
