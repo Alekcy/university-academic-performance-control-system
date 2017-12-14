c7832b347e304ec2c424ce9f0b72f1aaa2d1871c
Unknown <alekcyvolkov@gmail.com>
2017-12-14 22:22:27 +0300

    add printing academic performance

--- views/academic-performance/index.php
+++ views/academic-performance/index.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use \yii2assets\printthis\PrintThis;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\Search\AcademicPerformanceSearch */
@@ -17,6 +18,28 @@ $this->params['breadcrumbs'][] = $this->title;
     <p>
         <?= Html::a('Добавить', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
+    <?=
+     PrintThis::widget([
+        'htmlOptions' => [
+            'id' => 'PrintThis',
+            'btnClass' => 'btn btn-info',
+            'btnId' => 'btnPrintThis',
+            'btnText' => 'Печать',
+            'btnIcon' => 'fa fa-print'
+        ],
+        'options' => [
+            'debug' => false,
+            'importCSS' => true,
+            'importStyle' => false,
+            'loadCSS' => "print.css",
+            'pageTitle' => "",
+            'removeInline' => false,
+            'printDelay' => 333,
+            'header' => null,
+            'formValues' => true,
+        ]
+    ]);
+    ?>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
@@ -39,4 +62,25 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\ActionColumn'],
         ],
     ]); ?>
+    <div id="PrintThis">
+        <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
+
+                'chairName',
+                'id_Chair',
+                'id_Teacher',
+                'id_Reporting_type',
+                'id_Mark',
+                // 'id_Subject',
+                // 'id_student',
+                // 'id_group',
+                // 'id_faculty',
+                // 'id_speciality',
+                // 'Date',
+                // 'Hours_count',
+            ],
+        ]); ?>
+    </div>
 </div>
